54acb60d032c2a51c752d096864f5e3cf4512925
Ahmad Mizan Nur Haq <155483988+ahmadmizann@users.noreply.github.com>
2025-03-26 05:51:08 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Finish } from "./screens/Finish";
+import { ThemeProvider } from "./components/ui/theme-provider";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <Finish />
-  </StrictMode>,
+    <ThemeProvider defaultTheme="light" storageKey="smollpng-theme">
+      <Finish />
+    </ThemeProvider>
+  </StrictMode>
 );
