fd607087a442ee20cd51afe66f60a7dedfbc08e2
Ahmad Mizan Nur Haq <155483988+ahmadmizann@users.noreply.github.com>
2025-03-28 14:31:07 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
     "app/**/*.{ts,tsx}",
@@ -75,5 +77,4 @@ module.exports = {
     container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
   },
   plugins: [],
-  darkMode: ["class"],
 };
