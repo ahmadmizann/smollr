a8d14e3b7c612c9186436a69dd291ab365ac41f0
Ahmad Mizan Nur Haq <155483988+ahmadmizann@users.noreply.github.com>
2025-04-01 06:54:49 +0800

    Update Finish.tsx

--- src/screens/Finish/Finish.tsx
+++ src/screens/Finish/Finish.tsx
@@ -315,7 +315,7 @@ export const Finish = (): JSX.Element => {
           )}
 
           {/* Ko-fi Widget */}
-          <div className="w-full mt-8 rounded-lg overflow-hidden dark:bg-gray-800">
+          <div className="w-full mt-8 rounded-lg dark:bg-gray-800">
             <iframe
             id='kofiframe'
             src='https://ko-fi.com/ahmadmizanh/?hidefeed=true&widget=true&embed=true&preview=true'
